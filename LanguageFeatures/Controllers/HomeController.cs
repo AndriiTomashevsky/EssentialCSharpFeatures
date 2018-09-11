b6d0471ddc7be818205aa4d3a86f547c1bf9a7ec
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-11 15:13:27 +0300

    Using Extension Methods

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeatures.Controllers
 {
@@ -6,24 +7,10 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
-            decimal total = 0;
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
 
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i])
-                {
-                    case decimal decimalValue: // This case statement matches any decimal value and assigns it to a new variable called decimalValue.
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50: // This case statement matches int values and assigns them to a variable called intValue, 
-                                                          //but only when the value is greater than 50.
-                        total += intValue;
-                        break;
-                }
-            }
-
-            return View("Index", new string[] { $"Total: {total:C2}" });
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
         }
     }
 }
