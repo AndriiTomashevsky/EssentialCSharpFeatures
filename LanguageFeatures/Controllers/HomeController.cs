4a89dbc502e170d20381db1bfd11a0d678bdbe57
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-27 20:36:18 +0300

    Using Lambda Expression Methods and Properties

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -6,9 +6,6 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
-        {
-            return View(Product.GetProducts().Select(p => p?.Name));
-        }
+        public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
     }
 }
